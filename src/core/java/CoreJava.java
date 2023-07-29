f665d43e5ba8f19e62b7b23dee51916c7c088c9c
antro ranjani <antroranjani2001@gmail.com>
2023-07-29 14:33:51 +0530

    first file

---
+++ src/core/java/CoreJava.java
@@ -0,0 +1,13 @@
+package core.java;
+
+public class CoreJava {
+
+        public static void main(String[] args) {
+        
+        System.out.println("welcome");
+        System.out.println("welcome to my page");
+            System.out.println("hello welcome to the world");
+            System.out.println("java class");                                    
+        }
+    
+}
